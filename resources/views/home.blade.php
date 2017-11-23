b5a41acdbf92e0766205719ca977cc96957a7748
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2017-11-23 09:55:19 +0530

    initial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.index')
+
+@section('topcontent')
+	@component('components.home-top') @endcomponent
+@endsection
+
+@section('middlecontent')
+	@component('components.home-middle') @endcomponent
+@endsection
+
+@section('bottomcontent')
+	@component('components.home-bottom') @endcomponent
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,8 @@
+<html lang="{{ app()->getLocale() }}">
+<head>
+ @component('layouts.head') @endcomponent
+</head>
+<body>
+ @component('layouts.body') @endcomponent
+</body>
+</html>
